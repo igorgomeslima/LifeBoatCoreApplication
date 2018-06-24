99fd6fd63d08f18a092b9b3c2963d1cdb0397e1e
Igor Gomes de Lima <igorgomeslima@hotmail.com.br>
2018-06-24 12:23:49 -0300

    Adding a Configuration Source

--- LifeBoatCoreApplication/Startup.cs
+++ LifeBoatCoreApplication/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LifeBoatCoreApplication
@@ -18,7 +19,7 @@ namespace LifeBoatCoreApplication
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configuration)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +28,8 @@ namespace LifeBoatCoreApplication
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var defaultResponseMessage = configuration["DefaultResponseMessage"];
+                await context.Response.WriteAsync(defaultResponseMessage);
             });
         }
     }
