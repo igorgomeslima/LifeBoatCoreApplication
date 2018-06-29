903e00cf07ff48d605868e382de5862b42f94464
Igor Gomes de Lima <igorgomeslima@hotmail.com.br>
2018-06-29 17:27:34 -0300

    Implementing a DbContext.

---
+++ LifeBoatCoreApplication/Data/LifeBoatCoreApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LifeBoatCoreApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeBoatCoreApplication.Data
+{
+    public class LifeBoatCoreApplicationDbContext : DbContext
+    {
+        public LifeBoatCoreApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}   
---
+++ LifeBoatCoreApplication/Services/Implementations/SqlRestaurantData.cs
@@ -0,0 +1,37 @@
+﻿using LifeBoatCoreApplication.Data;
+using LifeBoatCoreApplication.Models;
+using LifeBoatCoreApplication.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeBoatCoreApplication.Services.Implementations
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly LifeBoatCoreApplicationDbContext _context;
+
+        public SqlRestaurantData(LifeBoatCoreApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Restaurants.Add(restaurant);
+            _context.SaveChanges();
+            return restaurant;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(fo => fo.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.OrderBy(ob => ob.Name);
+        }
+    }
+}
